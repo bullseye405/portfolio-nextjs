41051c71743d39514cabe4f489efbcab1212162f
Samir Prajapati <bullseye405@gmail.com>
2025-12-12 15:18:01 +0545

    clean up

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,16 +1,6 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 
 import AppFooter from '@/components/app-footer';
 import AppHeader from '@/components/app-header';
-import { ThemeProvider } from '@/components/theme-provider';
-
-const inter = Inter({ subsets: ['latin'] });
-
-export const metadata: Metadata = {
-  title: 'Samir Prajapati',
-  description: 'Personal Website',
-};
 
 export default function RootLayout({
   children,
--- app/resume/layout.tsx
+++ app/resume/layout.tsx
@@ -1,18 +1,8 @@
-import { Analytics } from '@vercel/analytics/react';
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-
-const inter = Inter({ subsets: ['latin'] });
-
-export const metadata: Metadata = {
-  title: 'Samir Prajapati',
-  description: 'Personal Website',
-};
 
 export default function ResumeLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <main className={inter.className}>{children}</main>;
+  return <main>{children}</main>;
 }
